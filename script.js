8cc3d6e53420a7706029f0241baed27efcd2a44b
shakhwat <raselbosss989@gmail.com>
2025-12-28 12:53:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+console.log('BuilderFlow clone initialized');
+
+// Navbar scroll effect (optional enhancement later)
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    // Logic to possibly shrink navbar or change opacity on scroll
+});
